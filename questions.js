05d5e1dedb88ab59b9041e5ce5ebaf935cb81312
Nirosha-A <85603546+Nirosha-A@users.noreply.github.com>
2021-06-15 12:22:35 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of Objects
+const quiz=[
+ {
+   q:'MS-Word is an example of ___________.',
+   options:['An operating system','A processing device','Application software','An input device'],
+   answer:2
+ },
+ {
+   q:'National Income estimates in India are prepared by __________.',
+   options:['Planning Commission','Reserve Bank of India','Central statistical organisation',' Indian statistical Institute'],
+   answer:2
+ },
+ {
+   q:'Ctrl, Shift and Alt are called _________ keys.',
+   options:['modifier','function','alphanumeric','adjustment'],
+   answer:0
+ },
+ {
+   q:'The staple food of the Vedic Aryan was ________',
+   options:['Barley and rice','Milk and its products','Rice and pulses','Vegetables and fruits'],
+   answer:1
+ },
+ {
+   q:'The tropic of cancer does not pass through which of these Indian states ?',
+   options:['Madhya Pradesh','West Bengal','Rajasthan','Odisha'],
+   answer:3
+ }
+]
